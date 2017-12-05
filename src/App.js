23e28365366d45e437c8ebe54a5e53dcb6a63477
Bruno Carvalho <dipsnatchy26@gmail.com>
2017-12-05 17:22:17 -0600

    Mini done

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
+    this.state =  {
       vehiclesToDisplay: [],
       buyersToDisplay: [],
       baseUrl: 'https://joes-autos.herokuapp.com'
